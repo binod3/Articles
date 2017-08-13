6d2072120839be3a5437ab4dab1a48733c836541
Binod <you@example.com>
2017-08-13 17:40:24 +0530

    signup

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
-	validates :name, presence: true,length:{ minimum: 2},format: { with: /\A[a-zA-Z]+\z/,
+	validates :name, presence: true, length:{ minimum: 3}, format: { with: /\A[a-zA-Z_]+(?: [a-zA-Z_]+)+(?: [a-zA-Z_]+)?\z/,
     message: "only allows letters" }
-     validates :password, confirmation: true, length: { in: 6..20 }, presence: true
+     validates :password, confirmation: true, length: { in: 6..20 }, presence: true,format: { with: /\A^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}\z/, message: " Minimum characters, at least one uppercase letter, one lowercase letter, one number and one special character  " }
      validates :password_confirmation, presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	#validates :email, presence: true,format: {with: VALID_EMAIL_REGEX},
@@ -9,7 +9,10 @@ class Article < ApplicationRecord
                 :length => {:minimum => 3, :maximum => 254},
                 :uniqueness => true,
                 :format => {with: VALID_EMAIL_REGEX}
-       validates :email_confirmation, presence: true         
+       validates :email_confirmation, presence: true
+       validates :state, presence: true, inclusion: { in: %w(Delhi Bihar U.P West Bengal),
+    message: "%{value} is not a valid state" }
+       validates :gender, presence: true         
       validates :dob, presence: true
       validates :phone,presence: true,:length =>{:is =>10},numericality: { only_integer: true }
       validates :terms_of_service, acceptance: true
